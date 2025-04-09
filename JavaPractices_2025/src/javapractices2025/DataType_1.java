86eef54844cba5e5dc7ac7cc88b42ab60be6cdc4
unknown <priya.konemane@gmail.com>
2025-04-09 12:40:54 +0530

    DataTypeOfJava

---
+++ JavaPractices_2025/src/javapractices2025/DataType_1.java
@@ -0,0 +1,64 @@
+package javapractices2025;
+
+public class DataType_1 {
+
+	public static void main(String[] args) {
+		
+		//byte	
+		byte m=10;
+		byte a=80;
+		System.out.println(a);
+		System.out.println(m);
+		
+		//short 
+		
+		short s=129;
+		System.out.println(s);
+		short s1=3099;
+		System.out.println("s1 value is"+ s1);
+		s1=278;
+		System.out.println("s1 valu now is"+ s1);
+		
+		//int
+		int i=72372;
+		System.out.println(i);
+		
+		//long :: we use l for represent the long 
+		long l=999799797979l;
+		System.out.println(l);
+		
+		//float 
+		
+		float f=1223f;
+		System.out.println(f);
+		
+		float f1=(float)123;
+		System.out.println(f1);
+		float f2=100;
+		System.out.println(f2);
+		
+		//double
+		
+		double d=23.346566565656566565;			
+		System.out.println(d);
+		
+		//char 
+		
+		char c='a';
+		System.out.println(c);		
+		char c1='$';
+		System.out.println(c1);
+		char c2='1';
+		System.out.println(c2);
+		System.out.println('u' +10);
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
